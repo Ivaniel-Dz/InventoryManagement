bd52265b0725f2a571ef53e216aa9030e2235ef5
Ivaniel-Dz <pcivaniel@gmail.com>
2024-10-17 00:00:10 -0500

    update notificacion modal

--- InventoryManagementWebApp/obj/Debug/net8.0/InventoryManagementWebApp.AssemblyInfo.cs
+++ InventoryManagementWebApp/obj/Debug/net8.0/InventoryManagementWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+081286e30909ef2afcc0313c21f0a599bb97bd52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620173debc0d40cdfe2c4d02b7f74edf8ef4e9c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
